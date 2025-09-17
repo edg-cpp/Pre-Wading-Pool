a5d45941122cdd0b93e1c6d3511a989e1369e9d1
Gokturk <edgokturk@protonmail.com>
2025-09-17 15:39:26 +0200

    3.5 c'est fini

--- Day8/hangman/main.py
+++ Day8/hangman/main.py
@@ -13,7 +13,17 @@ while True:
             pygame.quit()
             raise SystemExit
         
-        
         screen.fill("black")
         screen.blit(ball, ballrect)
-        pygame.display.flip()
+        #tete
+        pygame.draw.circle(screen,color=(0, 0, 0),center=[300, 150], radius=50,width=3)
+        #corps
+        pygame.draw.line(screen,color=(0,0,0),start_pos=[300,200],end_pos=[300,350],width=3)
+        #jambes
+        pygame.draw.line(screen,color=(0,0,0),start_pos=[300,350],end_pos=[200,450],width=3)
+        pygame.draw.line(screen,color=(0,0,0),start_pos=[300,350],end_pos=[400,450],width=3)
+        #bras
+        pygame.draw.line(screen,color=(0,0,0),start_pos=[300,240],end_pos=[400,290],width=3)
+        pygame.draw.line(screen,color=(0,0,0),start_pos=[300,240],end_pos=[200,290],width=3)
+        
+        pygame.display.update()
