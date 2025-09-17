03c86a4c5d65169ff9f6e59cf53d4196bc48ab48
Gokturk <edgokturk@protonmail.com>
2025-09-17 14:58:56 +0200

    task 3.4 terminé

--- Day8/hangman/main.py
+++ Day8/hangman/main.py
@@ -3,9 +3,17 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
 
+ball = pygame.image.load("backround.jpg")
+ballrect = ball.get_rect()
+
 while True:
     # Process player inputs.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
+        
+        
+        screen.fill("black")
+        screen.blit(ball, ballrect)
+        pygame.display.flip()
