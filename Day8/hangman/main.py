aa73dc2d1a8f2a022c3ecbaa40ca82fa42a26848
Gokturk <edgokturk@protonmail.com>
2025-09-17 14:37:55 +0200

    3.3 fait

--- Day8/hangman/main.py
+++ Day8/hangman/main.py
@@ -2,3 +2,10 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
+
+while True:
+    # Process player inputs.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
