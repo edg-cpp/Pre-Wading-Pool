a1a697d34ee85971b2e8c2040193af91378f4347
Gokturk <edgokturk@protonmail.com>
2025-09-18 22:54:35 +0200

    meilleure lisibilité

--- Day8/hangman_pyg/texts.py
+++ Day8/hangman_pyg/texts.py
@@ -8,10 +8,13 @@ font_30 = pygame.font.Font("sources/pixel.ttf", 30)
 font_50 = pygame.font.Font("sources/pixel.ttf", 50)
 font_60 = pygame.font.Font("sources/pixel.ttf", 60)
 
+# fond d'écran
 image = pygame.image.load("sources/backround.jpg")
 image_rect = image.get_rect()
 
-main_color = (255, 255, 255)
+# couleurs
+white = (255, 255, 255)
+red = (255, 0, 0)
 
 # coordinates
 centre = (300, 300)
@@ -21,9 +24,9 @@ centre_bas = (300, 400)
 
 def opening_screen(screen):
 
-    intro1 = font_60.render("HANGMAN", True, main_color)
-    intro2 = font_30.render("CLICK TO START", True, main_color)
-    # intro3 = font_50.render("ESC TO QUIT", True, main_color)
+    intro1 = font_60.render("HANGMAN", True, white)
+    intro2 = font_30.render("CLICK TO START", True, white)
+    # intro3 = font_50.render("ESC TO QUIT", True, white)
 
     introRect1 = intro1.get_rect()
     introRect2 = intro2.get_rect()
@@ -39,7 +42,7 @@ def opening_screen(screen):
 
 
 def initial_message(screen):
-    text = font_30.render("Please enter a letter: ", True, main_color)
+    text = font_30.render("Please enter a letter: ", True, white)
     textRect = text.get_rect()
     textRect.center = centre_haut
     screen.blit(text, textRect)
@@ -47,28 +50,28 @@ def initial_message(screen):
 
 def show_penalties(screen, pen):
     str = f"Penalties : {pen}"
-    text = font_15.render(str, True, main_color)
+    text = font_15.render(str, True, white)
     textRect = text.get_rect()
     textRect.center = (300, 100)
     screen.blit(text, textRect)
 
 
 def show_hidden_word(screen, hword):
-    text = font_50.render(hword, True, main_color)
+    text = font_50.render(hword, True, white)
     textRect = text.get_rect()
     textRect.center = centre
     screen.blit(text, textRect)
 
 
 def repetition_error(screen):
-    text = font_30.render("You already guessed this", True, (255, 0, 0))
+    text = font_30.render("You already guessed this", True, red)
     textRect = text.get_rect()
     textRect.center = centre_bas
     screen.blit(text, textRect)
 
 
 def letter_error(screen):
-    text = font_30.render("You must enter a letter", True, (255, 0, 0))
+    text = font_30.render("You must enter a letter", True, red)
     textRect = text.get_rect()
     textRect.center = centre_bas
     screen.blit(text, textRect)
@@ -80,7 +83,7 @@ def guessed_letters(screen, list, coor):
     str_guessed = ""
     for e in set(list):
         str_guessed += e + " "
-    text = font_30.render(str_guessed, True, main_color)
+    text = font_30.render(str_guessed, True, white)
     textRect = text.get_rect()
     textRect.center = coor
     screen.blit(text, textRect)
@@ -88,9 +91,9 @@ def guessed_letters(screen, list, coor):
 
 def win_text(screen, word):
     screen.blit(image, image_rect)
-    text = font_50.render("YOU WON", True, (255, 0, 0))
+    text = font_50.render("YOU WON", True, red)
     str = "The word was:" + word
-    text2 = font_30.render(str, True, main_color)
+    text2 = font_30.render(str, True, white)
     textRect = text.get_rect()
     text2Rect = text2.get_rect()
     textRect.center = centre_haut
@@ -101,9 +104,9 @@ def win_text(screen, word):
 
 def lose_text(screen, word):
     screen.blit(image, image_rect)
-    text = font_50.render("You Lost :(", True, (255, 0, 0))
+    text = font_50.render("You Lost :(", True, red)
     str = "The word was:" + word
-    text2 = font_30.render(str, True, main_color)
+    text2 = font_30.render(str, True, white)
     textRect = text.get_rect()
     text2Rect = text2.get_rect()
     textRect.center = centre_haut
@@ -113,7 +116,7 @@ def lose_text(screen, word):
 
 
 def print_check(screen, txt):
-    text = font_30.render(txt, True, main_color)
+    text = font_30.render(txt, True, white)
     textRect = text.get_rect()
     textRect.center = (300, 500)
     screen.blit(text, textRect)
